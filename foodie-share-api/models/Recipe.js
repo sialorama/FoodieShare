c12d841d1a98a2fe9f45dc0fb34b53e753970138
sialorama <sialorama@gmail.com>
2024-10-29 14:31:57 +0100

    Add Dockerfile for the API Update Docker-compose and

--- foodie-share-api/models/Recipe.js
+++ foodie-share-api/models/Recipe.js
@@ -3,10 +3,15 @@ const mongoose = require('mongoose');
 const recipeSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: String,
-    ingredients: [String],
-    instructions: [String],
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    likes: { type: Number, default: 0 },
+    ingredients: [String],        // Liste des ingrédients
+    steps: [String],              // Liste des étapes de préparation
+    comments: [                   // Liste des commentaires
+        {
+            user: String,             // Nom de l'utilisateur qui a commenté
+            message: String,          // Contenu du commentaire
+            date: { type: Date, default: Date.now }
+        }
+    ]
 });
 
 module.exports = mongoose.model('Recipe', recipeSchema);
